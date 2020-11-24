32b2b217a525a83800719da37eaa7abac9c7633d
imagire <imagire@gmail.com>
2020-11-25 06:26:34 +0900

    add files

---
+++ src/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 3.0f;
+	GameObject sword;
+
+    void Start()
+    {
+        sword = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Swipe();
+    }
+
+    private void Swipe()
+    {
+        if(Input.GetMouseButton(0))
+        {
+        	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        	Vector3 target_pos = ray.GetPoint(5.0f);
+        	
+        	sword.transform.LookAt(target_pos);
+        }
+    }
+}
